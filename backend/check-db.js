37e9d71f41a1fabb2870ddafa579921e9effa2fc
shunya-karan <karanyad9072@gmail.com>
2026-09-04 23:10:58 +0530

    Backup before Prisma upgrade

--- backend/check-db.js
+++ backend/check-db.js
@@ -1,19 +1,42 @@
 import Prisma from './src/database/connection.js';
 
 try {
-  const user = await Prisma.user.create({
-    data: {
-      userId: 'ADMIN001',
-      name: 'Admin User',
-      passwordHash: 'admin123',
-      role: 'ADMIN',
+  const users = await Prisma.user.findMany({
+    select: {
+      id: true,
+      userId: true,
+      name: true,
+      passwordHash: true,
+      role: true,
       isActive: true,
+      deletedAt: true,
     },
   });
 
-  console.log('User created successfully:', user);
+  console.table(users);
 } catch (error) {
   console.error(error);
 } finally {
   await Prisma.$disconnect();
 }
+
+
+
+// import Prisma from './src/database/connection.js';
+
+// try {
+//   const users = await Prisma.user.create({
+//   data: {
+//     userId: 'ADMIN001',
+//     name: 'Admin User',
+//     passwordHash: 'admin123',
+//     role: 'ADMIN',
+//     isActive: true,
+//   },
+// });
+//   console.table(users);
+// } catch (error) {
+//   console.error(error);
+// } finally {
+//   await Prisma.$disconnect();
+// }
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 export const connectDB = () => {
   try {
     const prisma = new PrismaClient();
