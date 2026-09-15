0490e4f23c0ac1209e70da763a4e0416e98a5a84
Karan <Karanthakur8385@gmail.com>
2026-09-15 20:39:47 +0530

    Mac/Win

--- frontend/lib/features/common/customer_intake/widgets/recent_entries_table.dart
+++ frontend/lib/features/common/customer_intake/widgets/recent_entries_table.dart
@@ -1,5 +1,5 @@
 ﻿import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+// import 'package:lucide_icons/lucide_icons.dart';
 import '../../../../../core/theme/app_colors.dart';
 
 // DATA MODEL (Backend ke liye ready) Backend aane par JSON ko isme map karna hoga
